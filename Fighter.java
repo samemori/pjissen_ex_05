8d7b7606c7fad5833b2d5a46d41cd36c9ec820c6
samemori <150803062+samemori@users.noreply.github.com>
2023-11-21 10:39:19 +0900

    Update Fighter.java

--- Fighter.java
+++ Fighter.java
@@ -6,7 +6,7 @@ class Fighter extends MovingObject {
     boolean dflag;
     boolean sflag;
     int delaytime;
-
+    Image img = getToolkit().getImage("img/Fighter.png");
 
     Fighter(int apWidth, int apHeight) {
 
@@ -14,8 +14,8 @@ class Fighter extends MovingObject {
         y = apHeight*0.9;
         dx = 0;
         dy = 0;
-        w = 10;
-        h = 10;
+        w = img.getWidth(this)/2;
+        h = img.getHeight(this)/2;
         lflag = false;
         rflag = false;
         uflag = false;
@@ -30,65 +30,66 @@ class Fighter extends MovingObject {
     }
 
     void move(Graphics buf, int apWidth, int apHeight) {
-    buf.setColor(Color.blue);
-    buf.fillRect((int)(x) - w, (int)(y) - h, 2 * w, 2 * h);
-    
-    boolean nomoveflg = true;
 
-    if (lflag && !rflag)
-        if (dx > -6) {
-            dx = dx - 2;
-            nomoveflg = false;
-        }
-    if (rflag && !lflag)
-        if (dx < 6) {
-            dx = dx + 2;
-            nomoveflg = false;
-        }
-    if (nomoveflg == true)
-        if (sflag) {
-            dx = dx * 0.8;
-        }
-        else {
-            dx = dx * 0.9;
-        }
-    
-    if (dx < 0 && x > w)
-        x = x + dx;
-    if (dx > 0 && x < apWidth - w)
-        x = x + dx;
-    
+        buf.drawImage(img, (int)(x) - w, (int)(y) - h, this);
+        w = img.getWidth(this)/2;
+        h = img.getHeight(this)/2;
+
+        boolean nomoveflg = true;
+
+        if (lflag && !rflag)
+            if (dx > -6) {
+                dx = dx - 2;
+                nomoveflg = false;
+            }
+        if (rflag && !lflag)
+            if (dx < 6) {
+                dx = dx + 2;
+                nomoveflg = false;
+            }
+        if (nomoveflg == true)
+            if (sflag) {
+                dx = dx * 0.8;
+            }
+            else {
+                dx = dx * 0.9;
+            }
+        
+        if (dx < 0 && x > w)
+            x = x + dx;
+        if (dx > 0 && x < apWidth - w)
+            x = x + dx;
+        
 
 
 
-    nomoveflg = true;
+        nomoveflg = true;
 
-    if (uflag && !dflag)
-        if (dy > -6) {
-            dy = dy - 2;
-            nomoveflg = false;
-        }
-    if (dflag && !uflag)
-        if (dy < 6) {
-            dy = dy + 2;
-            nomoveflg = false;
-        }
-    if (nomoveflg == true)
-        if (sflag) {
-            dy = dy * 0.8;
-        }
-        else {
-            dy = dy * 0.9;
-        }
+        if (uflag && !dflag)
+            if (dy > -6) {
+                dy = dy - 2;
+                nomoveflg = false;
+            }
+        if (dflag && !uflag)
+            if (dy < 6) {
+                dy = dy + 2;
+                nomoveflg = false;
+            }
+        if (nomoveflg == true)
+            if (sflag) {
+                dy = dy * 0.8;
+            }
+            else {
+                dy = dy * 0.9;
+            }
 
-    if (dy < 0 && y > h)
-        y = y + dy;
-    if (dy > 0 && y < apHeight - h)
-        y = y + dy;
+        if (dy < 0 && y > h)
+            y = y + dy;
+        if (dy > 0 && y < apHeight - h)
+            y = y + dy;
 
 
     
     }
     
-
 }
